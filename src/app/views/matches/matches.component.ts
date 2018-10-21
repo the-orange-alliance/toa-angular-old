38d49ba88509d8eef823f676f1b9c5a7a69d2c62
Ofek Ashery <ofek.ashery@gmail.com>
2018-10-22 00:14:41 +0300

    New match page

--- src/app/views/matches/matches.component.ts
+++ src/app/views/matches/matches.component.ts
@@ -4,6 +4,8 @@ import { FTCDatabase } from '../../providers/ftc-database';
 import Match from '../../models/Match';
 import Event from '../../models/Event';
 import { TheOrangeAllianceGlobals } from '../../app.globals';
+import {SafeResourceUrl} from "@angular/platform-browser/src/security/dom_sanitization_service";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'toa-match',
@@ -16,8 +18,9 @@ export class MatchesComponent implements OnInit {
 
   match: Match;
   event: Event;
+  videoSafeURL: SafeResourceUrl;
 
-  constructor(private ftc: FTCDatabase, private router: Router, private route: ActivatedRoute, private app: TheOrangeAllianceGlobals) {
+  constructor(private ftc: FTCDatabase, private router: Router, private sanitizer: DomSanitizer, private route: ActivatedRoute, private app: TheOrangeAllianceGlobals) {
     this.matchKey = this.route.snapshot.params['match_key'];
   }
 
@@ -25,6 +28,14 @@ export class MatchesComponent implements OnInit {
     this.ftc.getMatchDetails(this.matchKey).then((match: Match) => {
       if (match) {
         this.match = match;
+
+        if (match.videoURL != null) {
+          let videoID = match.videoURL.match(/^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/);
+          if (videoID && videoID[2].length == 11) {
+            this.videoSafeURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + videoID[2]);
+          }
+        }
+
         this.ftc.getEventBasic(match.eventKey).then((event: Event) => {
           this.event = event;
           this.app.setTitle(this.match.matchName + ' - ' + this.event.eventName);
