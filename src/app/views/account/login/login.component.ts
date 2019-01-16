21d70cac0651e055c94a88505a9c7c13c65ff936
Soren Zaiser <soren.zaiser@gmail.com>
2019-01-16 17:27:15 -0500

    Still dont know what im doing but changed the login page again

--- src/app/views/account/login/login.component.ts
+++ src/app/views/account/login/login.component.ts
@@ -8,7 +8,9 @@ import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+
 })
 
 export class LoginComponent implements OnInit {
