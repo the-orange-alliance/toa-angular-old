4c40595da1d498cee7208d8912a944c59f13a9a4
Ofek Ashery <ofek.ashery@gmail.com>
2019-02-07 15:59:17 +0200

    Fix #262

--- src/app/components/event/event.item.component.ts
+++ src/app/components/event/event.item.component.ts
@@ -12,11 +12,12 @@ export class EventItemComponent {
   @Input() hideLiveBadge: boolean;
 
   isLive(): boolean {
+    let liveData: boolean = this.event.teamCount > 0 || this.event.matchCount > 0 ;
     let startValue: number = this.removeFractionalDay(new Date(this.event.startDate)).valueOf();
     let endValue: number   = this.removeFractionalDay(new Date(this.event.endDate)).valueOf();
     let todayValue: number = this.removeFractionalDay(new Date()).valueOf();
 
-    return !this.hideLiveBadge && (todayValue <= endValue && todayValue >= startValue);
+    return !this.hideLiveBadge && liveData && (todayValue <= endValue && todayValue >= startValue);
   }
 
   private removeFractionalDay(date: Date) {
