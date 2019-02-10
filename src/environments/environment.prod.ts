af5138b0c5f9856d751d917f53325d60dfd116a4
Noah H <noahholo@icloud.com>
2019-02-10 14:12:13 -0600

    Translation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: "AIzaSyCxK14f13kbyb0p1NRYnGoqmgSWFHJvdo8",
+    authDomain: "the-orange-alliance.firebaseapp.com",
+    databaseURL: "https://the-orange-alliance.firebaseio.com",
+    projectId: "the-orange-alliance",
+    storageBucket: "the-orange-alliance.appspot.com",
+    messagingSenderId: "495169296462"
   }
 };
