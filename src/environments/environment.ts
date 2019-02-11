aaac026632051a7352a33a5272287051301263be
Noah <noahholo@icloud.com>
2019-02-10 19:14:54 -0600

    Typo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCxK14f13kbyb0p1NRYnGoqmgSWFHJvdo8",
-    authDomain: "the-orange-alliance.firebaseapp.com",
-    databaseURL: "https://the-orange-alliance.firebaseio.com",
-    projectId: "the-orange-alliance",
-    storageBucket: "the-orange-alliance.appspot.com",
-    messagingSenderId: "495169296462"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
