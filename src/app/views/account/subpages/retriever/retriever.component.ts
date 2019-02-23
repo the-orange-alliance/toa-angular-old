98dfb616d05da20c2e36675a9aceac95b3880395
Ofek Ashery <ofek.ashery@gmail.com>
2019-02-23 23:29:05 +0200

    [Admin] WIP Retriever

--- src/app/views/account/subpages/retriever/retriever.component.ts
+++ src/app/views/account/subpages/retriever/retriever.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AppBarService } from '../../../../app-bar.service';
-import { CloudFunctions } from "../../../../providers/cloud-functions";
+import { CloudFunctions } from '../../../../providers/cloud-functions';
+import Event from '../../../../models/Event';
 
 @Component({
   selector: 'toa-account-retriever',
-  templateUrl: './retriever.component.html'
+  templateUrl: './retriever.component.html',
+  styleUrls: ['../../account.component.scss']
 })
 export class RetrieverComponent implements OnInit {
 
   @Input() firebaseUser: firebase.User;
+  newEvents: Event[] = null;
 
   constructor(private appBarService: AppBarService, private cloud: CloudFunctions) {
 
@@ -16,8 +19,13 @@ export class RetrieverComponent implements OnInit {
 
   ngOnInit() {
     this.appBarService.setTitle('Retriever');
-    this.cloud.eventsRetriever(this.firebaseUser).then((events) => {
-      console.log(events);
+    this.cloud.eventsRetriever(this.firebaseUser).then((events: any) => {
+      this.newEvents = events.new_events.map((result: any) => new Event().fromJSON(result));
+      console.log(this.newEvents);
     });
+  }
+
+  createEvents() {
+    // TODO
   }
 }
