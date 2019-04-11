bcd07262e6240ae495ce1a196fc4e8854b62a34b
Ofek Ashery <ofek.ashery@gmail.com>
2019-04-11 14:53:23 +0300

    Remove notes

--- server.ts
+++ server.ts
@@ -1,14 +1,10 @@
+import { enableProdMode } from '@angular/core';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { join } from 'path';
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-import {enableProdMode} from '@angular/core';
-// Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
-
 import express = require('express');
-;
-import {join} from 'path';
 (global as any).WebSocket = require('ws');
 (global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
@@ -17,14 +13,13 @@ enableProdMode();
 
 // Express server
 const app = express();
-
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+// Leave this as require() since this file is built Dynamically from webpack
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+// Universal express-engine
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModuleNgFactory,
   providers: [
@@ -35,19 +30,17 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
 
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
-// Server static files from /browser
+// Server the static files with cache
 app.get('*.*', express.static(DIST_FOLDER, {
   maxAge: '1y'
 }));
 
-// All regular routes use the Universal engine
+// Server the regular routes use the Universal engine
 app.get('*', (req, res) => {
   res.render('index', { req });
 });
 
-// Start up the Node server
+// Start up the server
 app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
+  console.log(`The app listening on http://localhost:${PORT}`);
 });
