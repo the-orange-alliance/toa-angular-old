84cdda3c737e7195470ca04abc0d8b7ec284ab60
Ofek Ashery <ofek.ashery@gmail.com>
2019-04-13 21:36:46 +0300

    Remove ModuleMapLoader

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { TheOrangeAllianceComponent } from './app.component';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule,
     ServerTransferStateModule
   ],
   bootstrap: [TheOrangeAllianceComponent]
