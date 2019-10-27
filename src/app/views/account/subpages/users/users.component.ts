13e743da50504948d4bec62ed4cedfaa07e1bab1
techno11 <soren.zaiser@gmail.com>
2019-10-27 15:21:40 -0400

    Add User Search
    because the user page took too long load before

--- src/app/views/account/subpages/users/users.component.ts
+++ src/app/views/account/subpages/users/users.component.ts
@@ -13,6 +13,9 @@ export class UsersComponent implements OnInit {
   @Input() user: User;
   selectedUser: User = null;
   users: User[] = [];
+  queriedUsers: User[] = [];
+  errorLoading = false;
+  query: string;
 
   constructor(private appBarService: AppBarService, private cloud: CloudFunctions) {
 
@@ -22,10 +25,18 @@ export class UsersComponent implements OnInit {
     this.appBarService.setTitle('Users');
     this.cloud.getAllUsers(this.user.firebaseUser).then((data) => {
       this.users = data;
-    });
+      this.errorLoading = false;
+    }).catch(() => this.errorLoading = true);
   }
 
   selectUser(user: User) {
     this.selectedUser = user;
+  }
+
+  queryUsers() {
+    const query = this.query && this.query.trim().length > 0 ? this.query.toLowerCase().trim() : null;
+    if (query) {
+      this.queriedUsers = this.users.filter(user => ( String(user.email).includes(query) || String(user.displayName).includes(query)));
+    }
   }
 }
