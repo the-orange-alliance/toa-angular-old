a657fe360967d9f990d0d4fbee2272cb653d051d
Brendan Bailey <baileybr@mail.gvsu.edu>
2019-11-21 18:59:42 -0500

    Regions Skeleton Mostly Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { AddDataComponent } from './views/add-data/add-data.component';
 import { LoginComponent } from './views/account/login/login.component';
 import { RegisterComponent } from './views/account/register/register.component';
 
+
 const donateUrl = 'https://www.paypal.com/donate/?token=6Mm_17HVz4hWmEp5RZS052WhAnucfA0pqv2arAjZHSlR-xdeyTZHx4zduqEV23zUGuMPom&country.x=US&locale.x=US';
 
 const routes: Routes = [
