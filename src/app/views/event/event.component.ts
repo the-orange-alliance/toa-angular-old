a54b434a070dcf5e0a0bf6bb78dffe5e95392b95
Soren Zaiser <soren.zaiser@gmail.com>
2019-11-27 18:44:47 -0500

    Fix Event 404

--- src/app/views/event/event.component.ts
+++ src/app/views/event/event.component.ts
@@ -79,7 +79,7 @@ export class EventComponent implements OnInit {
 
             const COOKIEKEY = 'event_notification_settings_info';
             if (messaging.isSupported() && !this.cookieService.check(COOKIEKEY) && this.eventKey.startsWith('1819-CMP-DET')) {
-              const snackbarRef = this.snackbar.open('Do you know you can get push notifications about results on real time? Just click on the star button.', 'Amazing!', {
+              const snackbarRef = this.snackbar.open('Do you know you can get push notifications about results in real time? Just click on the star button.', 'Amazing!', {
                 timeoutMs: 10000
               });
 
@@ -244,6 +244,7 @@ export class EventComponent implements OnInit {
           this.changeUrlNoRoute('teams');
         }
       }, (err) => {
+        this.router.navigate(['/not-found']);
         console.log(err);
       })
       }
